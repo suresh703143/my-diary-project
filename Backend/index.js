00483c4fa72912c4c1ea05ec3a572fca760a1f82
sureshkumar703 <sureshyalamanchi2005@gmail.com>
2026-01-07 22:35:48 +0530

    Import DB connection

--- Backend/index.js
+++ Backend/index.js
@@ -12,21 +12,19 @@ app.use(express.urlencoded({ extended: true }));
 
 const mysql = require("mysql2");
 
-const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT
-});
+import mysql from "mysql2";
+
+const db = mysql.createConnection(process.env.MYSQL_PUBLIC_URL);
 
 db.connect((err) => {
   if (err) {
-    console.error("Database connection failed:", err);
+    console.error("DB connection failed:", err);
   } else {
     console.log("Database connected successfully");
   }
 });
+
+export default db;
 
 module.exports = db;
 
