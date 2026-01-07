f39aea251987dbbfcf5dc3fec4b019c66065fb7e
sureshkumar703 <sureshyalamanchi2005@gmail.com>
2026-01-07 20:07:42 +0530

    Import DB connection

--- Backend/index.js
+++ Backend/index.js
@@ -1,9 +1,11 @@
+require("dotenv").config();
 const express = require('express');
 const cors = require('cors');
 const bcrypt = require('bcrypt');
 const mysql = require('mysql2')
-require("dotenv").config();
+
 const app = express();
+const db = require("./config/db"); 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
